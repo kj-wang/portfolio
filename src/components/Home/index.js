66166e2629753c24690331a9abf0b3e814453507
kj-wang <kjwangxd@gmail.com>
2023-11-10 18:28:48 -0500

    fourth commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
-import LogoTitle from '../../assets/images/logo-s.png'
+// import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss';
-import AnimatedLetters from '../AnimatedLetters';
+import AnimatedLetters from '../AnimatedLetters';ç
 import { useEffect, useState } from 'react';
-import Loader from 'react-loaders';
+// import Loader from 'react-loaders';
 
 
 const Home = () => {
